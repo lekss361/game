242be1c7da0fe859396739b84b4c586756d6c4d0
Aleksey K. <a.kondratyev@appcntr.ru>
2025-04-21 13:16:06 +0300

    add func

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-// file: main.go
 package main
 
 import (
@@ -6,6 +5,8 @@ import (
 	"sort"
 	"strings"
 )
+
+type ActionFunc func(g *Game, args ...string) string
 
 type Person struct {
 	BackPack  bool
@@ -17,6 +18,7 @@ type Game struct {
 	Areas    map[string]*Area
 	Player   Person
 	DoorOpen bool
+	Commands map[string]ActionFunc
 }
 
 type Area struct {
@@ -25,6 +27,7 @@ type Area struct {
 	Neighbors   map[string]*Area
 	Objects     map[string]string
 	UseActions  map[string]map[string]func(*Game) string
+	LookAction  ActionFunc
 }
 
 var game Game
@@ -77,11 +80,53 @@ func initGame() {
 		},
 	}
 
+	defaultLook := func(g *Game, _ ...string) string {
+		return fmt.Sprintf(
+			"ничего интересного. можно пройти - %s",
+			neighborsList(g.Current),
+		)
+	}
+	kitchen.LookAction = func(g *Game, _ ...string) string {
+		places := describePlaces(g.Current.Objects)
+		suffix := "надо идти в универ."
+		if !g.Player.BackPack {
+			suffix = "надо собрать рюкзак и идти в универ."
+		}
+		return fmt.Sprintf(
+			"ты находишься на кухне, %s, %s можно пройти - %s",
+			places, suffix, neighborsList(g.Current),
+		)
+	}
+	room.LookAction = func(g *Game, _ ...string) string {
+		if len(g.Current.Objects) == 0 {
+			return fmt.Sprintf(
+				"пустая комната. можно пройти - %s",
+				neighborsList(g.Current),
+			)
+		}
+		places := describePlaces(g.Current.Objects)
+		return fmt.Sprintf(
+			"%s. можно пройти - %s",
+			places, neighborsList(g.Current),
+		)
+	}
+	corridor.LookAction = defaultLook
+	street.LookAction = defaultLook
+
+	commands := map[string]ActionFunc{
+		"осмотреться": func(g *Game, _ ...string) string { return g.Current.LookAction(g) },
+		"идти":        func(g *Game, args ...string) string { return g.Current.Go(g, args) },
+		"надеть":      func(g *Game, args ...string) string { return g.Current.Wear(g, args) },
+		"взять":       func(g *Game, args ...string) string { return g.Current.Take(g, args) },
+		"применить":   func(g *Game, args ...string) string { return g.Current.Use(g, args) },
+	}
+
 	game = Game{
 		Current:  kitchen,
 		Areas:    map[string]*Area{"кухня": kitchen, "коридор": corridor, "комната": room, "улица": street},
 		Player:   Person{BackPack: false, Inventory: make(map[string]int)},
 		DoorOpen: false,
+		Commands: commands,
 	}
 }
 
@@ -90,76 +135,26 @@ func handleCommand(cmd string) string {
 	if len(parts) == 0 {
 		return "неизвестная команда"
 	}
-	switch parts[0] {
-	case "осмотреться":
-		return cmdLook()
-	case "идти":
-		if len(parts) < 2 {
-			return "неизвестная команда"
-		}
-		return cmdGo(parts[1])
-	case "надеть":
-		if len(parts) < 2 {
-			return "неизвестная команда"
-		}
-		return cmdWear(parts[1])
-	case "взять":
-		if len(parts) < 2 {
-			return "неизвестная команда"
-		}
-		return cmdTake(parts[1])
-	case "применить":
-		if len(parts) < 3 {
-			return "неизвестная команда"
-		}
-		return cmdUse(parts[1], parts[2])
-	default:
+	action, ok := game.Commands[parts[0]]
+	if !ok {
 		return "неизвестная команда"
 	}
+	return action(&game, parts[1:]...)
 }
 
-func cmdLook() string {
-	area := game.Current
-	switch area.Name {
-	case "кухня":
-		places := describePlaces(area.Objects)
-		suffix := "надо идти в универ."
-		if !game.Player.BackPack {
-			suffix = "надо собрать рюкзак и идти в универ."
-		}
-		return fmt.Sprintf(
-			"ты находишься на кухне, %s, %s можно пройти - %s",
-			places, suffix, neighborsList(area),
-		)
-	case "комната":
-		if len(area.Objects) == 0 {
-			return fmt.Sprintf(
-				"пустая комната. можно пройти - %s",
-				neighborsList(area),
-			)
-		}
-		places := describePlaces(area.Objects)
-		return fmt.Sprintf(
-			"%s. можно пройти - %s",
-			places, neighborsList(area),
-		)
-	default:
-		return fmt.Sprintf(
-			"ничего интересного. можно пройти - %s",
-			neighborsList(area),
-		)
+func (a *Area) Go(g *Game, args []string) string {
+	if len(args) < 1 {
+		return "неизвестная команда"
 	}
-}
-
-func cmdGo(dest string) string {
-	next, ok := game.Current.Neighbors[dest]
+	dest := args[0]
+	next, ok := a.Neighbors[dest]
 	if !ok {
 		return "нет пути в " + dest
 	}
-	if dest == "улица" && !game.DoorOpen {
+	if dest == "улица" && !g.DoorOpen {
 		return "дверь закрыта"
 	}
-	game.Current = next
+	g.Current = next
 	return fmt.Sprintf(
 		"%s. можно пройти - %s",
 		next.Description,
@@ -167,49 +162,55 @@ func cmdGo(dest string) string {
 	)
 }
 
-func cmdWear(item string) string {
-	area := game.Current
+func (a *Area) Wear(g *Game, args []string) string {
+	if len(args) < 1 {
+		return "неизвестная команда"
+	}
+	item := args[0]
 	if item == "рюкзак" {
-		if _, ok := area.Objects["рюкзак"]; ok {
-			game.Player.BackPack = true
-			delete(area.Objects, "рюкзак")
+		if _, ok := a.Objects["рюкзак"]; ok {
+			g.Player.BackPack = true
+			delete(a.Objects, "рюкзак")
 			return "вы надели: рюкзак"
 		}
 	}
 	return "нет такого"
 }
 
-func cmdTake(item string) string {
-	area := game.Current
-	if !game.Player.BackPack {
+func (a *Area) Take(g *Game, args []string) string {
+	if len(args) < 1 {
+		return "неизвестная команда"
+	}
+	item := args[0]
+	if !g.Player.BackPack {
 		return "некуда класть"
 	}
-	if _, ok := area.Objects[item]; !ok {
+	if _, ok := a.Objects[item]; !ok {
 		return "нет такого"
 	}
-	game.Player.Inventory[item]++
-	delete(area.Objects, item)
+	g.Player.Inventory[item]++
+	delete(a.Objects, item)
 	return "предмет добавлен в инвентарь: " + item
 }
 
-func cmdUse(item, target string) string {
-	if game.Player.Inventory[item] == 0 {
+func (a *Area) Use(g *Game, args []string) string {
+	if len(args) < 2 {
+		return "неизвестная команда"
+	}
+	item, target := args[0], args[1]
+	if g.Player.Inventory[item] == 0 {
 		return "нет предмета в инвентаре - " + item
 	}
-	area := game.Current
-	if acts, ok := area.UseActions[item]; ok {
+	if acts, ok := a.UseActions[item]; ok {
 		if action, ok2 := acts[target]; ok2 {
-			return action(&game)
+			return action(g)
 		}
 	}
 	return "не к чему применить"
 }
 
 func describePlaces(objects map[string]string) string {
-	loc := map[string]string{
-		"стол": "столе",
-		"стул": "стуле",
-	}
+	loc := map[string]string{"стол": "столе", "стул": "стуле"}
 	byPlace := make(map[string][]string, len(objects))
 	for obj, place := range objects {
 		byPlace[place] = append(byPlace[place], obj)
@@ -219,7 +220,6 @@ func describePlaces(objects map[string]string) string {
 		places = append(places, p)
 	}
 	sort.Strings(places)
-
 	var parts []string
 	for _, p := range places {
 		items := byPlace[p]
